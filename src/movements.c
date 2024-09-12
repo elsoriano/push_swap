441576254a49cd168e30378630c25d0e39c8ff5f
Rodrigo Hernandez De Miguel <rhernand@c3r10s5.42malaga.com>
2024-09-12 20:17:19 +0200

    rotate *

--- src/movements.c
+++ src/movements.c
@@ -6,7 +6,7 @@
 /*   By: rhernand <rhernand@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/12 19:41:16 by rhernand          #+#    #+#             */
-/*   Updated: 2024/09/12 20:10:17 by rhernand         ###   ########.fr       */
+/*   Updated: 2024/09/12 20:14:56 by rhernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int ft_rotate(t_stack **stack, char c)
 		return (-1);
 	aux = (*stack)->next;
 	ft_lstadd_back_sw(stack, *stack);
-	*stack = *aux;
+	*stack = aux;
 	write(1, "r", 1);
 	write(1, &c, 1);
 	write(1, "\n", 1);
