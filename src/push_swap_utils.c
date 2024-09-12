070dee5eff084b68e1eb069559c586d8e9b2f605
rhernand <rhernand@student.42malaga.com>
2024-09-12 16:05:24 +0200

    *utils.c

--- src/push_swap_utils.c
+++ src/push_swap_utils.c
@@ -6,11 +6,28 @@
 /*   By: rhernand <rhernand@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/11 13:03:21 by rhernand          #+#    #+#             */
-/*   Updated: 2024/09/11 13:33:23 by rhernand         ###   ########.fr       */
+/*   Updated: 2024/09/11 14:53:45 by rhernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/push_swap.h"
+
+t_stack	*ft_lstnew_sw(int number, int index)
+{
+	t_stack	*node;
+
+	node = malloc(sizeof(t_stack));
+	if (!node)
+		return (NULL);
+	node->number = number;
+	node->index = index;
+	node->init_pos = -1;
+	node->target_pos = -1;
+	node->cost_a = -1;
+	node->cost_b = -1;
+	node->next = NULL;
+	return (node);
+}
 
 int	ft_atoi_sw(const char *str)
 {
@@ -21,9 +38,12 @@ int	ft_atoi_sw(const char *str)
 	sign = 1;
 	n = 0;
 	i = 0;
-	while (str[i] == ' ' || str[i] == '\n' || str[i] == '\t'
-		|| str[i] == '\v' || str[i] == '\f' || str[i] == '\r')
+	while (str[i])
+	{
+		if ((str[i] < 0 || str[i] > 9) && (str[i] != '+' && str[i] != '-'))
+			ft_error_exit(NULL, NULL);
 		i++;
+	}
 	if (str[i] == '-' || str[i] == '+')
 	{
 		if (str[i] == '-')
