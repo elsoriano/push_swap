fea71e048c3e866e913cf0aaf61c5fdb109b25b8
Rodrigo Hernandez De Miguel <rhernand@c3r8s5.42malaga.com>
2024-09-14 12:23:34 +0200

    ss & rr

--- src/movements.c
+++ src/movements.c
@@ -6,13 +6,13 @@
 /*   By: rhernand <rhernand@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/12 19:41:16 by rhernand          #+#    #+#             */
-/*   Updated: 2024/09/14 12:08:12 by rhernand         ###   ########.fr       */
+/*   Updated: 2024/09/14 12:22:32 by rhernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/push_swap.h"
 
-void ft_rotate(t_stack **stack_a, char c)
+void	ft_rotate(t_stack **stack_a, char c)
 {
 	t_stack	*aux;
 
@@ -36,11 +36,15 @@ void	ft_swap(t_stack **stack, char c)
 	aux = (*stack)->next;
 	aux->next = *stack;
 	(*stack)->next = (*stack)->next->next;
-	write(1, "s", 1);
-	write(1, &c, 1);
-	write(1, "\n", 1);
+	if (c != 's')
+	{
+		write(1, "s", 1);
+		write(1, &c, 1);
+		write(1, "\n", 1);
+	}
 	return ;
 }
+
 void	ft_push(t_stack **src, t_stack **dst, char c)
 {
 	t_stack	*aux;
@@ -56,4 +60,17 @@ void	ft_push(t_stack **src, t_stack **dst, char c)
 	write(1, &c, 1);
 	write(1, "\n", 1);
 	return ;
+}
+
+void	ft_ss(t_stack **stack_a, t_stack **stack_b)
+{
+	ft_swap(stack_a, 's');
+	ft_swap(stack_b, 's');
+	write(1, "ss\n", 3);
+}
+void	ft_rr(t_stack **stack_a, t_stack **stack_b)
+{
+	ft_rotate(stack_a, 'r');
+	ft_rotate(stack_b, 'r');
+	write(1, "rr\n", 3);
 }
