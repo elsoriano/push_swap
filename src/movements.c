ac6cc7525e54c80a2eeaac4a2bca2784fde938a6
Rodrigo Hernandez De Miguel <rhernand@c3r8s5.42malaga.com>
2024-09-14 11:42:07 +0200

    ft_swap

--- src/movements.c
+++ src/movements.c
@@ -6,18 +6,18 @@
 /*   By: rhernand <rhernand@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/12 19:41:16 by rhernand          #+#    #+#             */
-/*   Updated: 2024/09/12 20:14:56 by rhernand         ###   ########.fr       */
+/*   Updated: 2024/09/14 11:28:36 by rhernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/push_swap.h"
 
-int ft_rotate(t_stack **stack, char c)
+void ft_rotate(t_stack **stack_a, char c)
 {
 	t_stack	*aux;
 
 	if (!stack || *stack)
-		return (-1);
+		return ;
 	aux = (*stack)->next;
 	ft_lstadd_back_sw(stack, *stack);
 	*stack = aux;
@@ -25,4 +25,19 @@ int ft_rotate(t_stack **stack, char c)
 	write(1, &c, 1);
 	write(1, "\n", 1);
 	return (0);
+}
+
+void	ft_swap(t_stack **stack, char c)
+{
+	t_stack	*aux;
+
+	if (!stack || !*stack || !(*stack)->next)
+		return ;
+	aux = (*stack)->next;
+	aux->next = *stack;
+	(*stack)->next = (*stack)->next->next;
+	write(1, "s", 1);
+	write(1, &c, 1);
+	write(1, "\n", 1);
+	return ;
 }
