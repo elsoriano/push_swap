6e9b225e5aa849c3e95c7d766b248f17f62f97af
Rodrigo Hernandez De Miguel <rhernand@c3r8s5.42malaga.com>
2024-09-14 12:46:33 +0200

    rrr *compound

--- src/compound_movements.c
+++ src/compound_movements.c
@@ -6,7 +6,7 @@
 /*   By: rhernand <rhernand@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/14 12:27:44 by rhernand          #+#    #+#             */
-/*   Updated: 2024/09/14 12:27:47 by rhernand         ###   ########.fr       */
+/*   Updated: 2024/09/14 12:45:48 by rhernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,4 +22,10 @@ void	ft_rr(t_stack **stack_a, t_stack **stack_b)
 	ft_rotate(stack_a, 'r');
 	ft_rotate(stack_b, 'r');
 	write(1, "rr\n", 3);
+}
+void	ft_rrr(t_stack **stack_a, t_stack **stack_b)
+{
+	ft_rotate(stack_a, 'r');
+	ft_rotate(stack_b, 'r');
+	write(1, "rrr\n", 4);
 }
